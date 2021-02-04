a07189f8df82ea9a2021efb0b7fa08c0d149a0fb
Coenraad Human <coen.human@gmail.com>
2021-02-05 00:00:24 +0200

    Day 7: valid_parentheses problem.

---
+++ exercises/valid_parentheses/README.md
@@ -0,0 +1,18 @@
+## Problem
+
+[https://leetcode.com/problems/valid-parentheses/](https://leetcode.com/problems/valid-parentheses/)
+
+## Result
+
+```
+Runtime: 0 ms, faster than 100.00% of Rust online submissions for Valid Parentheses.
+Memory Usage: 2.1 MB, less than 32.10% of Rust online submissions for Valid Parentheses.
+```
+
+## Notes
+
+Missing if statement to check if stack is empty was forgotten leading to another submission.
+
+In addition, initial reading of problem was misunderstood as how the valid nested patterns look. I had to read solution to fully understand the problem at hand. Afterwards implementation was developed on it own.
+
+![all_results](all_results.png)
--- exercises/valid_parentheses/src/main.rs
+++ exercises/valid_parentheses/src/main.rs
@@ -3,16 +3,70 @@ pub fn is_valid(s: String) -> bool {
         return false;
     }
 
-    
+    let mut bracket_stack:Vec<char> = vec![];
+
+    for character in s.chars() {
+        match character {
+            '{' => bracket_stack.push('{'),
+            '[' => bracket_stack.push('['),
+            '(' => bracket_stack.push('('),
+            '}' => {
+                match bracket_stack.get(bracket_stack.len() - 1) {
+                    None => {
+                        return false;
+                    }
+                    Some('{') => {
+                        bracket_stack.pop();
+                    },
+                    Some(_) => {
+                        return false;
+                    }
+
+                }
+            },
+            ']' => {
+                match bracket_stack.get(bracket_stack.len() - 1) {
+                    None => {
+                        return false;
+                    }
+                    Some('[') => {
+                        bracket_stack.pop();
+                    },
+                    Some(_) => {
+                        return false;
+                    }
+
+                }
+            },
+            ')' => {
+                match bracket_stack.get(bracket_stack.len() - 1) {
+                    None => {
+                        return false;
+                    }
+                    Some('(') => {
+                        bracket_stack.pop();
+                    },
+                    Some(_) => {
+                        return false;
+                    }
+
+                }
+            },
+            _ => return false
+        }
+    }
+
+    if bracket_stack.len() > 0 {
+        return false;
+    }
 
     true
 }
 
 fn main() {
-    println!("Input: {}, result: {}.", String::from(""), is_valid(String::from("")));
-    println!("Input: {}, result: {}.", String::from(""), is_valid(String::from("")));
-    println!("Input: {}, result: {}.", String::from(""), is_valid(String::from("")));
-    println!("Input: {}, result: {}.", String::from(""), is_valid(String::from("")));
-    println!("Input: {}, result: {}.", String::from(""), is_valid(String::from("")));
-    println!("Input: {}, result: {}.", String::from(""), is_valid(String::from("")));
+    println!("Input: {}, result: {}.", String::from("()"), is_valid(String::from("()")));
+    println!("Input: {}, result: {}.", String::from("()[]{}"), is_valid(String::from("()[]{}")));
+    println!("Input: {}, result: {}.", String::from("(]"), is_valid(String::from("(]")));
+    println!("Input: {}, result: {}.", String::from("([)]"), is_valid(String::from("([)]")));
+    println!("Input: {}, result: {}.", String::from("{[]}"), is_valid(String::from("{[]}")));
 }
